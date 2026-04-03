0a25a216e4b612883655a75ebd0d6020ebaf91e4
Emily <emily.yao.220@gmail.com>
2026-04-02 22:46:23 -0500

    Update OnceUponAnArduino.ino

--- OnceUponAnArduino.ino
+++ OnceUponAnArduino.ino
@@ -9,18 +9,28 @@ void setup() {
   
 }
 
+//currently set for 360 servo
 void loop() {
   if (digitalRead (buttonPin) == HIGH) {
-    myservo.write(180);
+    myservo.write(180); //360 servo, speed
   } else{
     myservo.write(90);
   }
 }
 
+// for 180 servo, # in myservo.write() determines angle
+// for 360 servo, # in myservo.write() determines speed
+//use for servo loop 5 times
  //  for(int i = 0; i  < 5; i++){
   //   myservo.write(180);
   //   delay(100);
   //   myservo.write(0);
   //   delay(100);
+  // }
+//--------180/360 Servo------------------------
+//if (digitalRead (buttonPin) == HIGH) {
+  // myservo.write(180); //360 servo, speed
+  // } else{
+  //   myservo.write(90);
   // }
 
