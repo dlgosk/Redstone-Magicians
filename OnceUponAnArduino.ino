16836f34f6d82901950d480afce096d4febf372c
Emily <emily.yao.220@gmail.com>
2026-04-12 22:45:46 -0500

    Update OnceUponAnArduino.ino
    added clarification comments and removed comment at bottom

--- OnceUponAnArduino.ino
+++ OnceUponAnArduino.ino
@@ -16,6 +16,7 @@ void setup() {
 
 void loop() {
   //---FIRST SERVO------
+  // Mining tree servo; 180 servo
   if (digitalRead (buttonPin) == HIGH) {
     for (int i = 0; i < 5; i++) {
     myservo.write(180); 
@@ -28,6 +29,7 @@ void loop() {
   }
 
   //----SECOND SERVO------
+  // Nether portal servo; 360 servo
    if (digitalRead (buttonPin1) == HIGH) {
     secondServo.write(180);
   } else {
@@ -37,17 +39,4 @@ void loop() {
 
 // for 180 servo, # in myservo.write() determines angle
 // for 360 servo, # in myservo.write() determines speed
-//use for servo loop 5 times
- //  for(int i = 0; i  < 5; i++){
-  //   myservo.write(180);
-  //   delay(100);
-  //   myservo.write(0);
-  //   delay(100);
-  // }
-//--------180/360 Servo------------------------
-//if (digitalRead (buttonPin) == HIGH) {
-  // myservo.write(180); //360 servo, speed
-  // } else{
-  //   myservo.write(90);
-  // }
 
