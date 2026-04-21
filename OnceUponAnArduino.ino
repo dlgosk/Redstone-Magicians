e5a077f29800d73e2aa80d8c36298017aa111b85
Emily <emily.yao.220@gmail.com>
2026-04-20 23:20:12 -0500

    Update OnceUponAnArduino.ino
    Added the rest of the 4 servos and updated myServo to be called firstServo

--- OnceUponAnArduino.ino
+++ OnceUponAnArduino.ino
@@ -2,14 +2,35 @@
 
 Servo myservo;
 Servo secondServo;
-const int buttonPin = 2;
-const int buttonPin = 4;
+Servo thirdServo;
+Servo fourthServo;
+Servo fifthServo;
+Servo sixthServo;
+const int buttonPin1 = 2;
+const int buttonPin2 = 4;
+const int buttonPin3 = 3; // piglin servo
+const int buttonPin4 = 5; // crafting table 
+const int buttonPin5 = 6; // blaze 
+const int buttonPin6 = 7; // end portal
 
 void setup() {
-  myservo.attach(9);
+  firstServo.attach(9);
   pinMode(buttonPin, INPUT_PULLUP);
+  
   secondServo.attach(12);
   pinMode(buttonPin1, INPUT_PULLUP);
+  
+  thirdServo.attach(8); //piglin servo
+  pinMode(buttonPin3, INPUT_PULLUP);
+
+  fourthServo.attach(10); //crafting servo
+  pinMode(buttonPin3, INPUT_PULLUP);
+
+  fifthServo.attach(11); //blaze servo
+  pinMode(buttonPin3, INPUT_PULLUP);
+
+  sixthServo.attach(13);
+  pinMode(buttonPin3, INPUT_PULLUP);
   
 }
 
@@ -19,13 +40,13 @@ void loop() {
   // Mining tree servo; 180 servo
   if (digitalRead (buttonPin) == HIGH) {
     for (int i = 0; i < 5; i++) {
-    myservo.write(180); 
+    firstServo.write(180); 
     delay(500);
-    myservo.write(0);
+    firstServo.write(0);
     delay(500);
   }
   } else{
-    myservo.write(90);
+    firstServo.write(90);
   }
 
   //----SECOND SERVO------
@@ -36,6 +57,36 @@ void loop() {
     secondServo.write(90);
   }
 }
+
+//PIGLIN SERVO (3)
+  if (digitalRead (buttonPin3) == HIGH) {
+    thirdServo.write(180);
+    delay(5000);
+  } else {
+    thirdServo.write(90);
+  }
+
+ // CRAFTING SERVO (4)
+  if (digitalRead (buttonPin4) ==  HIGH) {
+    fourthServo.write(-170);
+    delay(5000);
+  } else {
+    fourthServo.write(90);
+  }
+
+  //360 servo: BLAZE SERVO (5)
+  if (digitalRead (buttonPin5) == HIGH) {
+    fifthServo.write(360);
+  } else {
+    fifthServo.write(90);
+  }
+
+  //360 servo: END PORTAL (6)
+  if (digitalRead (buttonPin6) ==  HIGH) {
+    sixthServo.write(70);
+  } else {
+    sixthServo.write(90);
+  }
 
 // for 180 servo, # in myservo.write() determines angle
 // for 360 servo, # in myservo.write() determines speed
